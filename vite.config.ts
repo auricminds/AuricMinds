cd81ed03f5e230551be03224e39afa1f2a181b29
OsamaHussein <osamahussein@Ussamas-Hussein.local>
2025-11-21 18:44:47 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/auricminds-website/",
+  base: '/AuricMinds/', // <-- must match your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
